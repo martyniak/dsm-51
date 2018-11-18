b01e8d5850e325d0ed90feeed8c36ee84b23f3c1
Marek Martyniak <ms.martyniak@gmail.com>
2018-11-18 22:10:50 +0100

    Compilation tests added

---
+++ test_compilation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# test if compilation ended with no errors
+
+function test {
+    if grep -Fxq "no errors" $1; then
+        echo "Test passed - $1"
+        return 0
+    else
+        echo "Test failed - $1"
+        cat $1
+        return 1
+    fi 
+}
+
+t1 = test test.lst
+t2 = test clock.lst
+
+if $t1 -ne 0 || $t2 -ne 0; then
+    exit 1
+fi
